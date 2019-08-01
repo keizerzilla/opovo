d0fe75bb197ade05f85399cf2472cda013926de3
keizerzilla <artur.rodrigues26@gmail.com>
2019-08-01 10:41:03 -0300

    agora abre pagina no navegador com estilo mais ou menos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # opovo.py
 Script que burla o paywall do jornal O Povo (www.opovo.com.br)
+
+## Requisitos
+beautifulsoup4 (`pip3 install --user beautifulsoup4`)
+
--- opovo.py
+++ opovo.py
@@ -3,39 +3,59 @@ opovo.py
 Artur Rodrigues Rocha Neto
 
 Script que burla o paywall do jornal O Povo (www.opovo.com.br)
-Requisitos: Python 3.x, BeautifulSoup
+Requisitos: Python 3.x, BeautifulSoup4
 """
 
 import sys
+import base64
+import tempfile
 import requests
+import webbrowser
 from bs4 import BeautifulSoup
 
+## checando numero de parametros
 if len(sys.argv) != 2:
 	print("Erro: número incorreto de argumentos!")
 	print("Uso: python3 opovo.py <ulr_da_noticia>")
 	sys.exit(1)
 
+## pegando URL passado por paramentro e fazendo conexao
 url = sys.argv[1]
-
 try:
 	page = requests.get(url)
 except:
-	print("Erro: página não encontrada!")
+	print("Erro: não foi possível carregar o recurso!")
 	print("Favor, tente novamente.")
 	sys.exit(1)
 
+## conferindo se tudo deu certo com a conexao
 if page.status_code != 200:
 	print("Erro: página não encontrada!")
 	print("Favor, tente novamente.")
 	sys.exit(1)
 
-soup = BeautifulSoup(page.content, "html.parser")
-text_content = soup.find_all("p", class_="texto")
+## carregando template de pagina a ser exibida no navegador
+template = open("template.html", "r")
+html_template = template.read()
+template.close()
 
-if len(text_content) == 0:
-	text_content = soup.find("div", class_="conteudo-interna")
-	print(text_content.get_text())
-else:
-	for text in text_content:
-		print(text.get_text())
-	
+## fazendo o parse da pagina e montagem do HTML resultado
+soup = BeautifulSoup(page.content, "html.parser")
+title = soup.title.string + " (opovo.py)"
+
+div_content = soup.find("div", class_="text-container")
+if div_content == None:
+	div_content = soup.find("div", class_="conteudo-interna")
+
+html = html_template.format(title, title, div_content)
+
+## criando pagina em arquivo temporario
+temp_page = tempfile.NamedTemporaryFile(delete=False)
+path = temp_page.name + ".html"
+f = open(path, "w")
+f.write(html)
+f.close()
+
+## exibindo conteudo que foi extraido
+webbrowser.open("file://" + path, new=2)
+
