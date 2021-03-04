a9c06e40ce073ca7c38f669e5ecf21b18396e0c4
Artur Rodrigues <artur.rodrigues26@gmail.com>
2021-03-04 20:53:20 -0300

    Versão atualizada (Telegraph)

--- opovo.py
+++ opovo.py
@@ -1,62 +1,145 @@
 """
 opovo.py
-Artur Rodrigues Rocha Neto
-
-Script que burla o paywall do jornal O Povo (www.opovo.com.br)
-Requisitos: Python 3.x, BeautifulSoup4
+autor:       Artur Rodrigues Rocha Neto
+contato:     artur.rodrigues26@gmail.com
+descrição:   Script que burla o paywall do jornal O Povo (www.opovo.com.br)
+uso:         Como bot Telegram ou como interface de linha de comando
+requisitors: beautifulsoup4, bleach, python-telegram-bot, telegraph_client
 """
 
 import sys
-import base64
-import tempfile
+import bs4
+import logging
 import requests
-import codecs
-import webbrowser
 from bs4 import BeautifulSoup
+from telegraph import Telegraph
+from telegram.ext import Updater
+from telegram.ext import Filters
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegraph.utils import html_to_content
 
-## checando numero de parametros
-if len(sys.argv) != 2:
-	print("Erro: número incorreto de argumentos!")
-	print("Uso: python3 opovo.py <ulr_da_noticia>")
-	sys.exit(1)
+TOKEN_BOT = "MEU_TOKEN_BOT_SHOW"
+TOKEN_TELEGRAPH = "MEU_TOKEN_TELEGRAPH_SHOW"
+LOGFORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
-## pegando URL passado por paramentro e fazendo conexao
-url = sys.argv[1]
-try:
-	page = requests.get(url)
-except:
-	print("Erro: não foi possível carregar o recurso!")
-	print("Favor, tente novamente.")
-	sys.exit(1)
+def parse_url(url):
+    """
+    """
+    
+    page = None
+    try:
+        page = requests.get(url)
+        if page.status_code != 200:
+            return "Não consegui quebrar essa página..."
+    except:
+        return "Não consegui quebrar essa página..."
+    
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    # procura titulo da materia
+    titulo = soup.find("h1", class_="article-title")
+    if titulo:
+        titulo = titulo.string.strip()
+    else:
+        titulo = soup.find("h1", class_="tit-noticia").string.strip()
+    
+    # procura pela manchete
+    manchete = soup.find("span", class_="article-desc")
+    if manchete:
+        manchete = manchete.string.strip()
+    else:
+        manchete = soup.find("h2", class_="abre-noticia").string.strip()
+    manchete = "<h4>" + manchete + "</h4>"
+    
+    # procura nome do autor
+    nome_autor = ""
+    autor = soup.find("span", class_="by-author")
+    if autor:
+        nome_autor = autor.find("a").string
+    else:
+        autor = soup.find("span", class_="colunista-author")
+        nome_autor = autor.string
+    
+    # formatacao do conteudo textual
+    conteudo = []
+    for paragrafo in soup.findAll("p", class_="texto"):
+        if paragrafo.string:
+            conteudo.append(paragrafo.string.strip())
+        else:
+            subcontent = [coisa.string for coisa in paragrafo.contents if coisa.string]
+            conteudo.append("".join(subcontent).strip())
+    
+    # procura div com o conteudo em si para raspar imagens
+    container = soup.find("div", class_="text-container")
+    if not container:
+        container = soup.find("div", class_="conteudo-interna")
+    images = container.findAll("figure")
+    if images:
+        images = [str(img) for img in images]
+    
+    # geracao do HTML resultado
+    content = [manchete] + ["<p>" + txt + "</p>" for txt in conteudo if len(txt) > 0]
+    if images:
+        content = content + images
+    content = "".join(content)
+    content = html_to_content(content)
+    
+    # publicacao via API do Telegraph
+    telegraph = Telegraph(TOKEN_TELEGRAPH, timeout=60)
+    new_page = telegraph.create_page(title=titulo, author_name=nome_autor, content=content)
+    
+    return new_page.url
 
-## conferindo se tudo deu certo com a conexao
-if page.status_code != 200:
-	print("Erro: página não encontrada!")
-	print("Favor, tente novamente.")
-	sys.exit(1)
+def start(update, context):
+    """
+    """
+    
+    txt = "Me envie um link do Portal O Povo que eu quebrarei o paywall para você. Leitura completa direta no Telegram."
+    context.bot.send_message(chat_id=update.effective_chat.id, text=txt)
 
-## carregando template de pagina a ser exibida no navegador
-template = open("template.html", "r")
-html_template = template.read()
-template.close()
+def echo(update, context):
+    """
+    """
+    
+    ans = parse_url(update.message.text)
+    context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=ans)
 
-## fazendo o parse da pagina e montagem do HTML resultado
-soup = BeautifulSoup(page.content, "html.parser")
-title = soup.title.string + " (opovo.py)"
+def cli(url):
+    """
+    """
+    
+    ans = parse_url(url)
+    
+    print()
+    print("-"*len(ans))
+    print(ans)
+    print("-"*len(ans))
+    print()
 
-div_content = soup.find("div", class_="text-container")
-if div_content == None:
-	div_content = soup.find("div", class_="conteudo-interna")
+def start_bot():
+    """
+    """
+    
+    # log de erros e avisos
+    logging.basicConfig(format=LOGFORMAT, level=logging.INFO)
+    
+    # objetos updater e dispatcher
+    updater = Updater(token=TOKEN_BOT, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    # handlers dos comandos
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    
+    # liga o bot
+    print("Preparado para quebrar paywalls...")
+    updater.start_polling()
+    updater.idle()
 
-html = html_template.format(title, title, div_content)
-
-## criando pagina em arquivo temporario
-temp_page = tempfile.NamedTemporaryFile(delete=False)
-path = temp_page.name + ".html"
-f = codecs.open(path, "w", "utf-8")
-f.write(html)
-f.close()
-
-## exibindo conteudo que foi extraido
-webbrowser.open("file://" + path, new=2)
-
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        cli(sys.argv[1])
+    else:
+        start_bot()
