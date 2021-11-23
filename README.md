a2d0396daf405a125b48141b8989d7f8f8db8a05
Artur Rodrigues Rocha Neto <artur.rodrigues26@gmail.com>
2021-11-22 22:01:01 -0300

    - README atualizado - Requisitos atualizados - Suporte a Telegram/Telegraph removidos - Retorno da função de raspagem agora é um dicionário (facilita construção futura de API)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# opovo_pwb
-`opovo_pwb` (*O Povo Paywall Breaker*) é um projeto que visa construir uma interface para raspagem de notícia do Jornal [O Povo](https://www.opovo.com.br/) de Fortaleza, Ceará. Pode ser usado como CLI (interface de linha de comando) ou como bot Telegram. Escrito em Python 3, o código necessita de alguns pacotes para funcionamento (arquivo `requirements.txt`), além de uma chave API para o serviço Telegraph; essas são os requistos obrigatórios ([instruções Telegraph](https://telegram.org/blog/telegraph)). Para uso como bot Telegram, que é uma funcionalidade opcional, uma bot token desse serviço é necessário ([instruções Telegram bot](https://core.telegram.org/bots#3-how-do-i-create-a-bot)).
+# opovo.py
+
+`opovo_pwb` (*O Povo Paywall Breaker*) é um projeto que visa construir uma interface para raspagem de notícia do Jornal [O Povo](https://www.opovo.com.br/) de Fortaleza, Ceará. Até o momento, permite salvar o conteúdo raspado em arquivo (uso padrão como script) ou visualizando na própria saída padrão (em construção: formatação adequada à quantidade de colunas disponíveis).
 
 ## Instalação de pré-requisitos
 
@@ -11,6 +12,9 @@ pip3 install --user --upgrade -r requirements.txt
 
 Se o seu sistema não reconhecer o compando pip3, tente trocá-lo para pip. Se o erro persistir, verifique como instalar o gerenciador de pacotes no seu sistema operacional.
 
-## Configurando chaves Telegraph e Telegram bot
+## Uso
 
-Nas primeiras linhas do código você encontrará duas variáveis: `TOKEN_TELEGRAPH` e `TOKEN_BOT`, que guardam as chaves de API pros serviços Telegraph e Telegram bot, respectivamente. Você deve trocar os valores temporários com as respectivas chaves que você criou seguindo as intruções linkadas no início deste README (lembrando que apenas o serviço Telegraph é obrigatório).
+```python
+python3 opovo.py <url_da_materia>
+```
+
--- opovo.py
+++ opovo.py
@@ -1,38 +1,28 @@
 """
-opovo.py
+arquivo:     opovo.py
 autor:       Artur Rodrigues Rocha Neto
 contato:     artur.rodrigues26@gmail.com
 descrição:   Script que burla o paywall do jornal O Povo (www.opovo.com.br)
-uso:         Como bot Telegram ou como interface de linha de comando
-requisitors: beautifulsoup4, bleach, python-telegram-bot, telegraph-client
+requisitors: beautifulsoup4
 """
 
 import sys
 import bs4
-import logging
 import requests
 from bs4 import BeautifulSoup
-from telegraph import Telegraph
-from telegram.ext import Updater
-from telegram.ext import Filters
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegraph.utils import html_to_content
-
-TOKEN_BOT = "MEU_TOKEN_BOT_SHOW"
-TOKEN_TELEGRAPH = "MEU_TOKEN_TELEGRAPH_SHOW"
-LOGFORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 def parse_url(url):
-    """
-    Raspa notícia por trás do paywall do Jornal O Povo.
+    """ Raspa notícia por trás do paywall do Jornal O Povo.
     
     Parâmetros
     ----------
         url: A url da notícia do O Povo.
+    
     Retorno
     -------
-        Link Telegraph para matéria raspada ou mensagem de erro.
+        ans: Dicionário com as três partes principais do conteúdo: título, autor
+        e conteúdo.
+    
     """
     
     page = None
@@ -60,15 +50,14 @@ def parse_url(url):
         manchete = manchete.string.strip()
     else:
         manchete = soup.find("h2", class_="abre-noticia").string.strip()
-    manchete = "<h4>" + manchete + "</h4>"
     
     # procura nome do autor
     # classes possiveis: by-author, colunista-author
-    nome_autor = soup.find("span", class_="by-author")
-    if nome_autor:
-        nome_autor = nome_autor.find("a").string
+    autor = soup.find("span", class_="by-author")
+    if autor:
+        autor = autor.find("a").string
     else:
-        nome_autor = soup.find("span", class_="colunista-author").string
+        autor = soup.find("span", class_="colunista-author").string
     
     # formatacao do conteudo textual
     conteudo = []
@@ -79,116 +68,80 @@ def parse_url(url):
             subcontent = [coisa.string for coisa in paragrafo.contents if coisa.string]
             conteudo.append("".join(subcontent).strip())
     
-    # geracao do HTML resultado
-    content = [manchete] + ["<p>" + txt + "</p>" for txt in conteudo if len(txt) > 0]
+    ans = {
+        "titulo"   : titulo,
+        "autor"    : autor,
+        "conteudo" : conteudo
+    }
     
-    # procura div com o conteudo em si para raspar imagens
-    container = soup.find("div", class_="text-container")
-    if not container:
-        container = soup.find("div", class_="conteudo-interna")
-    images = container.findAll("figure")
-    if images:
-        images = [str(img) for img in images]
-        content = content + images
-    
-    # gera HTML da pagina Telegraph
-    content = "".join(content)
-    content = html_to_content(content)
-    
-    # publicacao via API do Telegraph
-    try:
-        telegraph = Telegraph(TOKEN_TELEGRAPH, timeout=60)
-        new_page = telegraph.create_page(title=titulo, author_name=nome_autor, content=content)
-        return new_page.url
-    except:
-        return "Não foi possível gerar a página... Tente mais tarde!"
+    return ans
 
-def start(update, context):
-    """
-    Função padrão de um bot Telegram.
-    Responde com mensagem instruindo o usuário sobre o uso.
-    
-    Parâmetros
-    ----------
-        update: Objeto com estado atual da conversa.
-        context: Objeto com informações gerais da API.
-    Retorno
-    -------
-        Nenhum.
-    """
-    
-    txt = "Me envie um link do Portal O Povo que eu quebrarei o paywall para você. Leitura completa direta no Telegram."
-    context.bot.send_message(chat_id=update.effective_chat.id, text=txt)
-
-def echo(update, context):
-    """
-    Função que gerencia a interação com o bot.
-    Recebe URL, chama função de raspagem e retorna resultado (além de limpar conversa).
-    
-    Parâmetros
-    ----------
-        update: Objeto com estado atual da conversa.
-        context: Objeto com informações gerais da API.
-    Retorno
-    -------
-        Nenhum.
-    """
-    
-    ans = parse_url(update.message.text)
-    context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=ans)
 
 def cli(url):
-    """
-    Função da interface de linha de comando.
-    Chama função de raspagem e mostra URL final na saída padrão.
+    """ Função da interface de linha de comando. Chama função de raspagem e
+    mostra conteúdo raspado na saída padrão.
     
     Parâmetros
     ----------
         url: A url da notícia do O Povo.
+    
     Retorno
     -------
         Nenhum.
+    
     """
     
     ans = parse_url(url)
     
+    print(ans["titulo"].upper())
+    print(ans["autor"])
     print()
-    print("-"*len(ans))
-    print(ans)
-    print("-"*len(ans))
     print()
+    
+    for line in ans["conteudo"]:
+        if len(line) <= 80:
+            print(line)
+        else:
+            print(line[:80])
+            print(line[80:])
+        print()
 
-def start_bot():
-    """
-    Função de interface com bot.
+
+def to_txt(url, filename=None):
+    """ Função para salvar em arquivo texto. Chama função de raspagem e guarda
+    conteúdo raspado em arquivo nomeado por padrão com o mesmo nome da matéria.
     
     Parâmetros
     ----------
-        Nenhum.
+        url: A url da notícia do O Povo.
+        filename: Nome do arquivo onde salvar o conteúdo raspado.
+    
     Retorno
     -------
         Nenhum.
+    
     """
     
-    # log de erros e avisos
-    logging.basicConfig(format=LOGFORMAT, level=logging.INFO)
+    ans = parse_url(url)
     
-    # objetos updater e dispatcher
-    updater = Updater(token=TOKEN_BOT, use_context=True)
-    dispatcher = updater.dispatcher
+    if filename is None:
+        filename = ans["titulo"] + ".txt"
     
-    # handlers dos comandos
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
-    
-    # liga o bot
-    print("Preparado para quebrar paywalls...")
-    updater.start_polling()
-    updater.idle()
+    with open(filename, "w") as dump:
+        dump.write(ans["titulo"].upper())
+        dump.write("\n")
+        dump.write(ans["autor"])
+        dump.write("\n")
+        dump.write(f"Original: {url}\n")
+        dump.write("\n\n")
+        
+        dump.write("\n\n".join(ans["conteudo"]))
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        cli(sys.argv[1])
+        to_txt(sys.argv[1])
     else:
-        start_bot()
+        print("Número insuficiente de argumentos!")
+        print("USO: python3 opovo.py <url_da_matéria>")
+
+
